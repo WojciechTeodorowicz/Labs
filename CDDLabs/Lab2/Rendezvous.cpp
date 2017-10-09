370ee33995c810112f26c4e4beecf8384f71c1ea
Wojtek <wojtek@wojtek.vm>
2017-10-09 13:07:06 +0100

    working exe

--- CDDLabs/Lab2/Rendezvous.cpp
+++ CDDLabs/Lab2/Rendezvous.cpp
@@ -2,27 +2,27 @@
 #include <iostream>
 #include <thread>
 
-void ThreadA(std::shared_ptr<Semaphore> aArrived, std::shared_ptr<Semaphore> bArrived){
+void ThreadA(std::shared_ptr<Semaphore> SemA, std::shared_ptr<Semaphore> SemB){
   std::cout << "ThreadA: First A Thread" << std::endl;
-  aArrived->Singal();
-  bArrived->Wait();
+  SemA->Signal();
+  SemB->Wait();
 
   std::cout << "ThreadA: Second A Thread " << std::endl;
 }
-void ThreadB(std::shared_ptr<Semaphore> aArrived, std::shared_ptr<Semaphore> bArrived){
+void ThreadB(std::shared_ptr<Semaphore> SemA, std::shared_ptr<Semaphore> SemB){
   std::cout << "ThreadB : First B thread" << std::endl;
-  bArrived->Signal();
-  aArrived->Wait();
+  SemB->Signal();
+  SemB->Wait();
 
   std::cout << "ThreadB : Second B Thread" << std::endl;
 }
 int main(void){
 std::thread threadA, threadB;
-std::shared_ptr<Semaphore> aArrived( new Semaphore);
-std::shared_ptr<Semaphore> bArrived( new Semaphore);
+std::shared_ptr<Semaphore> SemA( new Semaphore);
+std::shared_ptr<Semaphore> SemB( new Semaphore);
 
-threadA=std::thread(ThreadA, aArrived, bArrived);
-threadB=std::thread(ThreadB, aArrived, bArrived);
+threadA=std::thread(ThreadA, SemA, SemB);
+threadB=std::thread(ThreadB, SemB, SemB);
 
   
  threadA.join();
